60b83800b163d7d8d3e8f985f3673248fdd111c1
lulongji <lulongji2011@163.com>
2018-07-09 14:11:40 +0800

    初始化

---
+++ src/main/java/com/llj/base/support/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.llj.base.support;
+
+import java.io.Serializable;
+
+
+public abstract class BaseEntity implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -250118731239275742L;
+
+}
